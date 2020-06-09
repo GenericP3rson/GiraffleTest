4c47b6a71b24c71c456c3371b6d6d4ea383ba530
Saber Nomen <genericp3rson@gmail.com>
2020-06-09 15:54:20 -0500

    Work after Workshop

---
+++ TryAgain/ModelData/build.gradle.kts
@@ -0,0 +1,11 @@
+import com.optum.giraffle.tasks.*
+import com.optum.giraffle.*
+
+plugins {
+    id("com.optum.giraffle") version "1.3.4"
+    id("net.saliman.properties") version "1.5.1"
+}
+
+repositories {
+    jcenter()
+}
