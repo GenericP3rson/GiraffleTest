2b90dfcb0c57317bc4358c755a147d0012f69569
Saber Nomen <genericp3rson@gmail.com>
2020-06-10 22:38:04 -0500

    Day 2 work

---
+++ TryAgain/TakeTwo/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.optum.giraffle") version "@project_version@"
+}
+
+val gHost: String by project
+val gAdminUserName: String by project
+val gAdminPassword: String by project
+val gUserName: String by project
+val gPassword: String by project
+val gGraphName: String by project
+val gClientVersion: String? by project
+
+val tokenMap: LinkedHashMap<String, String> = linkedMapOf("graphname" to gGraphName)
+
+tigergraph {
+    scriptDir.set(file("db_scripts"))
+    tokens.set(tokenMap)
+    serverName.set(gHost)
+    userName.set(gUserName)
+    password.set(gPassword)
+    adminUserName.set(gAdminUserName)
+    adminPassword.set(gAdminPassword)
+    gClientVersion?.let {
+        gsqlClientVersion.set(it)
+    }
+}
